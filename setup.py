a07534cfd6ac074fe852a72567bab21423f690fe
ui906879 <gibran.afzal@rwe.com>
2021-09-03 10:00:48 +0100

    feat: initial commit with small energy aspects api wrapper

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="oilanalytics",
+    version="0.0.1",
+    author="aeorxc",
+    description="Utilities for oil analytics",
+    url="https://github.com/aeorxc/oilanalytics",
+    project_urls={
+        "Source": "https://github.com/aeorxc/oilanalytics",
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["pandas", "requests", "commodutil"],
+    python_requires=">=3.8",
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+)
