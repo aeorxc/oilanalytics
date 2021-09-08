9bc959944c543e44324711d4753f8bd20992b9a4
ui906879 <gibran.afzal@rwe.com>
2021-09-08 17:26:24 +0100

    feat: add in EA tars file

---
+++ utils/fileutils.py
@@ -0,0 +1,1 @@
+import globimport osdef find_latest_file(fold):    list_of_files = glob.glob(fold)    list_of_files = [x for x in list_of_files if '~' not in x]    latest_file = max(list_of_files, key=os.path.getctime)    return latest_file
