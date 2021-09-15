90dc3731177547efe2d007db0e1a8377b55e5515
ui906879 <gibran.afzal@rwe.com>
2021-09-15 11:21:19 +0100

    fix: adding in start of summary tables

---
+++ oilanalytics/balances/balance_config.py
@@ -0,0 +1,1 @@
+global_oil_cols = [    'Demand',    'OECD',    'Non-OECD',    'Non-OPEC supply',    'North America',    'FSU',    'OPEC NGLs/Condensates',    'OPEC crude',    'Stock change']global_oil_basic_summary_cols = [    'Demand',    'Non-OPEC supply',    'OPEC crude',    'OPEC NGLs/Condensates',    'Stock change']
