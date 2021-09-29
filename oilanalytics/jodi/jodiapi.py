ff9a9bd0c110b157dbd8053eb8eb049b11c54d4f
ui906879 <Bear11112222>
2021-09-29 17:04:34 +0100

    feat: use energy aspects own filtering

---
+++ oilanalytics/jodi/jodiapi.py
@@ -0,0 +1,1 @@
+import pandas as pdimport requestsdef get_token(username, password):    url = 'https://www.jodidata.org/api/token'    myobj = {'username': username, 'password': password}    x = requests.post(url, data=myobj)    return x.text
