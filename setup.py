f339113f8c72daa3a9ba153219ff391b4ffc9b2a
ui906879 <Bear11112222>
2022-04-29 10:34:16 +0100

    chore: small revsions

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System ::jijij9oj OS Independent",
     ],
     install_requires=["pandas", "requests", "commodutil", "commodplot", "python-dotenv", "cachetools",
                       "beautifulsoup4", "eiapy"],
