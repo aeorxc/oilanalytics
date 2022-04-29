8647424ace3d82efd2ba5367f98fdaa696a3712c
ui906879 <Bear11112222>
2022-04-29 17:04:40 +0100

    chore: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="oilanalytics",
-    version="0.1.39",
+    version="0.2.0",
     author="aeorxc",
     description="Utilities for oil analytics",
     url="https://github.com/aeorxc/oilanalytics",
