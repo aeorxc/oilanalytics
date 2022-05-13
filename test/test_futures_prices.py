710e1b2e21b6aee15e702cb5c094e44629157e2e
ui906879 <Bear11112222>
2022-05-13 10:48:30 +0100

    chore: clean up doe weeklypetreport.py

--- test/test_futures_prices.py
+++ test/test_futures_prices.py
@@ -1,1 +1,1 @@
-from oilanalytics.prices import futures_pricesdef test_futures_structure():    res = futures_prices.futures_structure(start_date='date is within 5 days')    assert 'FB_M1' in res.columns
+from oilanalytics.prices import futures_pricesdef test_futures_structure():    res = futures_prices.futures_structure(start_date='date is within 5 days')    assert 'FB_M1' in res.columnsdef test_gen_symbol_page():    res = futures_prices.generate_symbol_page('FB', symbolname='Brent')    assert res is not None
