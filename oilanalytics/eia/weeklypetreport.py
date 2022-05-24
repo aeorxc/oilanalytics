cd0db2a5055ac07d25bfc23bdd70a7b58dda0a51
Owen Hulme <ui935441@rwe-ag.com>
2022-05-24 10:59:46 +0100

    revert

--- oilanalytics/eia/weeklypetreport.py
+++ oilanalytics/eia/weeklypetreport.py
@@ -7,8 +7,6 @@ import urllib3
 import re
 from oilanalytics.utils import chartutils as cu
 
-email_test_receivers = ['gibran.afzal@rwe.com', 'owen.hulme@rwe.com']
-
 fileloc = "https://ir.eia.gov/wpsr/psw09.xls"
 
 eia_url = "https://www.eia.gov/opendata/qb.php?sdid=PET.%s.W"
@@ -174,7 +172,7 @@ def gen_and_send_email(
         data=data,
         template="doe_weekly_email.html",
         package_loader_name="oilanalytics.eia",
-        receiver_email=email_test_receivers,
+        receiver_email=receiver_email,
         sender_email=sender_email,
     )
 
