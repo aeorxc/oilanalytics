cfed853d91c002b743e5e728e4562f3f15abc366
UI935452 <josh.alexander@rwe.com>
2022-11-01 15:17:18 +0000

    Changes to ea_macros template

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="oilanalytics",
-    version="0.2.22",
+    version="0.2.23",
     author="aeorxc",
     description="Utilities for oil analytics",
     url="https://github.com/aeorxc/oilanalytics",
