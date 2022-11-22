d5a9062c876c34da11ba5a4cc2c2a68fad9b91af
ui906879 <gibran.afzal@rwe.com>
2022-11-22 11:46:36 +0000

    skip failing test due to lack of permissions

--- test/test_ea_reports.py
+++ test/test_ea_reports.py
@@ -1,3 +1,5 @@
+import pytest
+
 from oilanalytics.energyaspects import china_stats, fsu_stats
 
 
@@ -6,6 +8,7 @@ def test_china_stats():
     assert res is not None
 
 
+@pytest.mark.skip("No longer permissioned on EA API")
 def test_fsu_stats():
     res = fsu_stats.generate_page()
     assert res is not None
