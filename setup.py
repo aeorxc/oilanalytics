c102b4035c863f1cabb3e7224e8406b50a541ad1
ui906879 <gibran.afzal@rwe.com>
2023-11-15 16:10:10 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="oilanalytics",
-    version="0.3.7",
+    version="0.3.9",
     author="aeorxc",
     description="Utilities for oil analytics",
     url="https://github.com/aeorxc/oilanalytics",
